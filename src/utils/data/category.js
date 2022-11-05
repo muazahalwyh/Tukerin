7deed168a7320d592d19ba354705d855fbd6be7f
maulana dimyati <dimyatimaulana@gmail.com>
2022-11-05 14:36:26 +0700

    category with static date

---
+++ src/utils/data/category.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    category: 'laptop',
+  },
+  {
+    category: 'kulkas',
+  },
+  {
+    category: 'handphone',
+  },
+  {
+    category: 'sofa',
+  },
+  {
+    category: 'pakaian',
+  },
+  {
+    category: 'sepatuk',
+  },
+  {
+    category: 'makanan',
+  },
+  {
+    category: 'kamera',
+  },
+];
+
+export default products;
