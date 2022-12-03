836bce5c22a8d556ec3c0d39e499306f5ebfebdc
maulana dimyati <dimyatimaulana@gmail.com>
2022-12-03 21:33:52 +0700

    new variable for idb

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,9 @@
 const CONFIG = {
   BASE_URL: 'https://sementara',
   BASE_IMAGE_URL: 'https://asset.kompas.com/crops/9e1_Ou4L-D2soeUpvKUx-sD8rVk=/0x0:1200x800/750x500/data/photo/2019/03/17/1964103890.jpg',
+  TUKERIN_IDB: 'tukerin-store',
+  DATABASE_VERSION: 1,
+  USER_STORE: 'authed-user',
 };
 
 export default CONFIG;
