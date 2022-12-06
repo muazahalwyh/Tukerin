dafac6be5cf234c37fff74bad87d3e8c139eb24a
maulana dimyati <dimyatimaulana@gmail.com>
2022-12-06 14:43:02 +0700

    add new meet out team section

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,16 +1,16 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
-import {
-  FaLinkedin, FaGithubSquare, FaLinkedinIn, FaGithub,
-} from 'react-icons/fa';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import logo from '../images/brand-tukerin.png';
-import kazuha from '../images/kazuha.jpeg';
 import kelebihanTukerin from '../images/homepage.png';
 import '../styles/AboutPage.css';
 import Loading from '../components/Loading';
 
-import ganyu from '../images/team/ganyu bg-modified.png';
+import alan from '../images/team/alan.png';
+import iqbal from '../images/team/iqbal.png';
+import muazah from '../images/team/muazah.png';
+import ajeng from '../images/team/ajeng.png';
 
 function AboutPage() {
   const [IsLoading, setIsLoading] = useState(true);
@@ -73,104 +73,51 @@ function AboutPage() {
           <h2 style={{ textAlign: 'center' }}>Meet the team</h2>
           <div className="container-team">
             <div className="member">
-              <div className="profile-picture1"><img src={ganyu} alt="profile img" /></div>
+              <div className="profile-picture1"><img src={iqbal} alt="profile img" /></div>
               <div className="member-info">
                 <h3>Muhammad Iqbal Hibatullah</h3>
                 <p>R012X0099</p>
                 <p>Bandung</p>
               </div>
               <div className="social-media">
-                <a style={{ color: '#0077b5' }} href="/"><FaLinkedinIn /></a>
-                <a href="/"><FaGithub /></a>
+                <a style={{ color: '#0077b5' }} href="https://www.linkedin.com/in/muhammad-iqbaal-hibatullah-a31324214/"><FaLinkedinIn /></a>
+                <a href="https://github.com/EvoVincere"><FaGithub /></a>
               </div>
             </div>
             <div className="member">
-              <div className="profile-picture1"><img src={ganyu} alt="profile img" /></div>
+              <div className="profile-picture1"><img src={alan} alt="profile img" /></div>
               <div className="member-info">
                 <h3>Maulana Dimyati</h3>
                 <p>R120X0181</p>
                 <p>Tegal</p>
               </div>
               <div className="social-media">
-                <a style={{ color: '#0077b5' }} href="/"><FaLinkedinIn /></a>
-                <a href="/"><FaGithub /></a>
+                <a style={{ color: '#0077b5' }} href="https://www.linkedin.com/in/maulanadimyati/"><FaLinkedinIn /></a>
+                <a href="https://github.com/dimyatimaulana"><FaGithub /></a>
               </div>
             </div>
             <div className="member">
-              <div className="profile-picture1"><img src={ganyu} alt="profile img" /></div>
+              <div className="profile-picture1"><img src={ajeng} alt="profile img" /></div>
               <div className="member-info">
                 <h3>Aliffiah Siti Ratuajeng</h3>
                 <p>R299Y0764</p>
                 <p>Bandung</p>
               </div>
               <div className="social-media">
-                <a style={{ color: '#0077b5' }} href="/"><FaLinkedinIn /></a>
-                <a href="/"><FaGithub /></a>
+                <a style={{ color: '#0077b5' }} href="https://www.linkedin.com/in/aliffiah-siti-ratuajeng-a55056256/"><FaLinkedinIn /></a>
+                <a href="https://github.com/Alifiasraa"><FaGithub /></a>
               </div>
             </div>
             <div className="member">
-              <div className="profile-picture1"><img src={ganyu} alt="profile img" /></div>
+              <div className="profile-picture1"><img src={muazah} alt="profile img" /></div>
               <div className="member-info">
                 <h3>Mu’azah Al’Adawiyah</h3>
                 <p>R429Y0975</p>
                 <p>Jambi</p>
               </div>
               <div className="social-media">
-                <a style={{ color: '#0077b5' }} href="/"><FaLinkedinIn /></a>
-                <a href="/"><FaGithub /></a>
-              </div>
-            </div>
-          </div>
-          <div className="about-team">
-            <h1>TENTANG KAMI</h1>
-            <div className="card-container">
-              <div className="card-item">
-                <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
-                <div className="card-info">
-                  <p className="card-info__name">Muhammad Iqbaal Hibatullah</p> <br />
-                  <p className="card-info__id">R012X0099</p> <br />
-                  <p className="card-info__city">Bandung</p> <br />
-                </div>
-                <div className="card-action">
-                  <a href="/"><FaLinkedin size={40} /></a>
-                  <a href="/"><FaGithubSquare size={40} /></a>
-                </div>
-              </div>
-              <div className="card-item">
-                <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
-                <div className="card-info">
-                  <p className="card-info__name">Maulana Dimyati</p> <br />
-                  <p className="card-info__id">R120X0181</p> <br />
-                  <p className="card-info__city">Tegal</p> <br />
-                </div>
-                <div className="card-action">
-                  <a href="/"><FaLinkedin size={40} /></a>
-                  <a href="/"><FaGithubSquare size={40} /></a>
-                </div>
-              </div>
-              <div className="card-item">
-                <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
-                <div className="card-info">
-                  <p className="card-info__name">Aliffiah Siti Ratuajeng</p> <br />
-                  <p className="card-info__id">R299Y0764</p> <br />
-                  <p className="card-info__city">Bandung</p> <br />
-                </div>
-                <div className="card-action">
-                  <a href="/"><FaLinkedin size={40} /></a>
-                  <a href="/"><FaGithubSquare size={40} /></a>
-                </div>
-              </div>
-              <div className="card-item">
-                <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
-                <div className="card-info">
-                  <p className="card-info__name">Mu’azah Al’Adawiyah</p> <br />
-                  <p className="card-info__id">R429Y0975</p> <br />
-                  <p className="card-info__city">Jambi</p> <br />
-                </div>
-                <div className="card-action">
-                  <a href="/"><FaLinkedin size={40} /></a>
-                  <a href="/"><FaGithubSquare size={40} /></a>
-                </div>
+                <a style={{ color: '#0077b5' }} href="https://www.linkedin.com/in/muazah-aladawiyah-4194741b9/"><FaLinkedinIn /></a>
+                <a href="https://github.com/muazahalwyh"><FaGithub /></a>
               </div>
             </div>
           </div>
@@ -181,3 +128,57 @@ function AboutPage() {
 }
 
 export default AboutPage;
+
+// <div className="about-team">
+//   <h1>TENTANG KAMI</h1>
+//   <div className="card-container">
+//     <div className="card-item">
+//       <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
+//       <div className="card-info">
+//         <p className="card-info__name">Muhammad Iqbaal Hibatullah</p> <br />
+//         <p className="card-info__id">R012X0099</p> <br />
+//         <p className="card-info__city">Bandung</p> <br />
+//       </div>
+//       <div className="card-action">
+//         <a href="/"><FaLinkedin size={40} /></a>
+//         <a href="/"><FaGithubSquare size={40} /></a>
+//       </div>
+//     </div>
+//     <div className="card-item">
+//       <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
+//       <div className="card-info">
+//         <p className="card-info__name">Maulana Dimyati</p> <br />
+//         <p className="card-info__id">R120X0181</p> <br />
+//         <p className="card-info__city">Tegal</p> <br />
+//       </div>
+//       <div className="card-action">
+//         <a href="/"><FaLinkedin size={40} /></a>
+//         <a href="/"><FaGithubSquare size={40} /></a>
+//       </div>
+//     </div>
+//     <div className="card-item">
+//       <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
+//       <div className="card-info">
+//         <p className="card-info__name">Aliffiah Siti Ratuajeng</p> <br />
+//         <p className="card-info__id">R299Y0764</p> <br />
+//         <p className="card-info__city">Bandung</p> <br />
+//       </div>
+//       <div className="card-action">
+//         <a href="/"><FaLinkedin size={40} /></a>
+//         <a href="/"><FaGithubSquare size={40} /></a>
+//       </div>
+//     </div>
+//     <div className="card-item">
+//       <div className="card-avatar"><img src={kazuha} alt="foto profil" /></div>
+//       <div className="card-info">
+//         <p className="card-info__name">Mu’azah Al’Adawiyah</p> <br />
+//         <p className="card-info__id">R429Y0975</p> <br />
+//         <p className="card-info__city">Jambi</p> <br />
+//       </div>
+//       <div className="card-action">
+//         <a href="/"><FaLinkedin size={40} /></a>
+//         <a href="/"><FaGithubSquare size={40} /></a>
+//       </div>
+//     </div>
+//   </div>
+// </div>;
